e518a1b74464a0ee880b7e57fa44c9bd5923b68e
Shaji Ahmed <schajee@gmail.com>
2023-01-19 15:16:43 +0500

    Fixed repository

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'Deploying'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:schajee/schajee.github.io.git master
+git push -f git@github.com:schajee/schajee.github.io.git HEAD:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
