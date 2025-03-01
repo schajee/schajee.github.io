443b2c7dfd87017a9a3a6026be9d2ce9dcaedfba
Shaji Ahmed <ahmeds@alphaxds.com>
2025-03-01 11:32:53 +0500

    Trying alternatives

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/schajee.github.io/'
-  },
-  ssr: false,
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   compatibilityDate: "2025-02-18",
