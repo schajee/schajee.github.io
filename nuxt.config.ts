0f1cf1ceea9b3d0a6258b6681420f38c27b1d16a
Shaji Ahmed <ahmeds@alphaxds.com>
2025-03-01 11:12:18 +0500

    Trying Copilot recommendations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/'
+    baseURL: '/schajee.github.io/'
   },
   ssr: false,
   devtools: { enabled: true },
